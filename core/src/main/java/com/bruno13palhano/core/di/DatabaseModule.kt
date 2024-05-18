82574a282fa9b7b8d25140c2b57eefcb3131e272
bruno13palhano <bruno13palhano@gmail.com>
2024-05-17 22:26:09 -0300

    Implement hilt module for database dependency injection

---
+++ core/src/main/java/com/bruno13palhano/core/di/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.bruno13palhano.core.di
+
+import android.content.Context
+import com.bruno13palhano.cache.AppDatabase
+import com.bruno13palhano.core.data.database.DatabaseFactory
+import com.bruno13palhano.core.data.database.DriverFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabaseFactoryDriver(@ApplicationContext context: Context): AppDatabase {
+        return DatabaseFactory(
+            driverFactory = DriverFactory(context = context)
+        ).createDriver()
+    }
+}
