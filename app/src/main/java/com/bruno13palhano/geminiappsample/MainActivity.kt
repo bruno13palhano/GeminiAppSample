c1abfcaf93eabd0f8692bdd40c1609df173f584f
bruno13palhano <bruno13palhano@gmail.com>
2024-05-18 19:42:39 -0300

    Make MainActiviy an entry point for dependency injection

--- app/src/main/java/com/bruno13palhano/geminiappsample/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/geminiappsample/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.bruno13palhano.geminiappsample.ui.navigation.AppNavGraph
 import com.bruno13palhano.geminiappsample.ui.theme.GeminiAppSampleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
