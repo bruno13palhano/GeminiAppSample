8b0bd9f1d444751fc807a1bbebfe08064bb98b92
bruno13palhano <bruno13palhano@gmail.com>
2024-05-20 23:42:44 -0300

    Add a enum for model types

--- core/src/main/java/com/bruno13palhano/core/data/repository/GenerativeModelRepository.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/GenerativeModelRepository.kt
@@ -1,19 +1,41 @@
 package com.bruno13palhano.core.data.repository
 
-import com.bruno13palhano.core.di.DefaultRandom
 import com.bruno13palhano.core.di.LessRandom
-import com.bruno13palhano.core.di.MoreRandom
+import com.bruno13palhano.core.model.ChatMessage
+import com.bruno13palhano.core.model.Participant
 import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.asTextOrNull
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-internal class GenerativeModelRepository @Inject constructor(
-    @LessRandom private val lessRandom: GenerativeModel,
-    @DefaultRandom private val defaultRandom: GenerativeModel,
-    @MoreRandom private val moreRandom: GenerativeModel
+internal class LessRandomModelRepository @Inject constructor(
+    @LessRandom private val generativeModel: GenerativeModel
 ) : Repository {
-    override fun lessRandomModel(): GenerativeModel = lessRandom
+    private val chat = generativeModel.startChat()
 
-    override fun defaultModel(): GenerativeModel = defaultRandom
+    override suspend fun messages(): Flow<List<ChatMessage>> {
+        return flow {
+            emit(
+                chat.history.map {
+                    ChatMessage(
+                        text = it.parts.first().asTextOrNull() ?: "",
+                        participant = if (it.role == "user") Participant.USER else Participant.MODEL,
+                        isPending = false
+                    )
+                }
+            )
+        }
+    }
 
-    override fun moreRandoModel(): GenerativeModel = moreRandom
+    override suspend fun sendMessage(message: String): String {
+        return try {
+            val response = chat.sendMessage(message)
+
+            response.text ?: ""
+        } catch (e: Exception) {
+            e.printStackTrace()
+            e.localizedMessage
+        }
+    }
 }
---
+++ core/src/main/java/com/bruno13palhano/core/model/ModelType.kt
@@ -0,0 +1,7 @@
+package com.bruno13palhano.core.model
+
+enum class ModelType(val value: String) {
+    LESS("LESS"),
+    DEFAULT("DEFAULT"),
+    MORE("MORE")
+}
